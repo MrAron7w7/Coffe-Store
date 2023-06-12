4c4539e6896b66d19c1a3a6bcca1ea6cc87ac80f
MrAron7w7 <magallanestorresaron@gmail.com>
2023-06-12 17:18:21 -0500

    Persoalizacion de el login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:coffestore/pages/HomePage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,10 +11,68 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
+      title: "Coffe Store",
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            // Separación entre los 3 componentes
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Coffe Store",
+                    style: TextStyle(
+                      color: Colors.brown,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "This is the best coffe Store",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color.fromARGB(255, 70, 66, 66),
+                    ),
+                  )
+                ],
+              ),
+              Image.asset('assets/coffe.gif'),
+              Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Iniciar Sesion'),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.brown,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(30))), // Minimo de tamaño
+                  ),
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Registrarse"),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.brown,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
-      routes: {'/': (context) => const HomePage()},
     );
   }
 }
